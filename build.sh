4af7a101aca653d76fcc96c2ffe4637b1ca37585
tigattack <10629864+tigattack@users.noreply.github.com>
2022-06-08 15:51:37 +0100

    Fixes and indenting

--- build.sh
+++ build.sh
@@ -2,54 +2,53 @@
 set -e
 
 if [ -z "$1" ]; then
-  read -p 'Enter your GitHub username: ' USER
+	read -p 'Enter your GitHub username: ' USER
 else
-  USER=$1
+	USER=$1
 fi
 if [ -z "$2" ]; then
-  echo "You need a GitHub token with the 'read:packages' scope to download packages."
-  echo 'You should generate a short-lived token here: https://github.com/settings/tokens/new?scopes=read:packages&description=Revanced'
-  read -sp 'Enter your GitHub token: ' TOKEN
+	echo "You need a GitHub token with the 'read:packages' scope to download packages."
+	echo 'You should generate a short-lived token here: https://github.com/settings/tokens/new?scopes=read:packages&description=Revanced'
+	read -sp 'Enter your GitHub token: ' TOKEN
+	echo ''
 else
-  TOKEN=$2
+	TOKEN=$2
 fi
 
 components=(
-  'revanced-integrations.apk'
-  'com.google.android.youtube_17.20.37.apk'
+	'revanced-integrations.apk'
+	'com.google.android.youtube_17.20.37.apk'
 )
 
 repos=(
-  'revanced-patcher'
-  'revanced-patches'
-  'revanced-cli'
+	'revanced-patcher'
+	'revanced-patches'
+	'revanced-cli'
 )
 
 for component in "${components[@]}" ; do
-  printf "\nChecking for file '%s'...\n" "$component"
-  if [ -f "./build/$component" ] ; then
-    printf "Found file '%s'!\n" "$component"
-  else
-    printf "Error: file '%s' does not exist. Please place it in ./builds/ and retry.\n" "$component"
-    exit 1
-  fi
+	printf "\nChecking for file '%s'...\n" "$component"
+	if [ -f "./build/$component" ] ; then
+		printf "Found file '%s'!\n" "$component"
+	else
+		printf "Error: file '%s' does not exist. Please place it in ./builds/ and retry.\n" "$component"
+		exit 1
+	fi
 done
 
 for repo in "${repos[@]}" ; do
-  printf "\nChecking for repo '%s'...\n" "$repo"
-  if [ -d "./build/$repo" ] ; then
-    printf "Found repo '%s'!\n" "$repo"
-  else
-    printf "Did not find repo '%s'. Cloning now...\n" "$repo"
-    git clone --quiet "https://github.com/revanced/$repo" "./build/$repo"
-  fi
+	printf "\nChecking for repo '%s'...\n" "$repo"
+	if [ -d "./build/$repo" ] ; then
+		printf "Found repo '%s'!\n" "$repo"
+	else
+		printf "Did not find repo '%s'. Cloning now...\n" "$repo"
+		git clone --quiet "https://github.com/revanced/$repo" "./build/$repo"
+	fi
 done
 
-if [[ -z "$(docker images -q revanced-builder)" ]]; then
-  printf '\nBuilding Docker image\n'
-  chmod +x entrypoint.sh
-  docker build --tag revanced-builder .
-fi
+printf '\nBuilding Docker image\n'
+chmod +x entrypoint.sh
+docker build --tag revanced-builder .
 
 printf '\nRunning Docker image\n'
 docker run --rm -v "$(pwd)/build":/build -e "GH_USER=$USER" -e "GH_TOKEN=$TOKEN" --name ReVanced-Builder revanced-builder
--- entrypoint.sh
+++ entrypoint.sh
@@ -10,31 +10,43 @@ echo "org.gradle.welcome=never" >> ~/.gradle/gradle.properties # Doesn't work?
 printf '\nDone.\n'
 
 printf '\n===============================\n== Building revanced-patcher ==\n===============================\n'
-cd /build/revanced-patcher || exit 1
-chmod +x ./gradlew
-printf '\nRunning build...\n'
-./gradlew publish
-printf '\nRelocating build asset(s)...\n'
-mv ./build/libs/revanced-patcher-*.jar /build/libs/
-printf '\nDone.\n'
+if ! ls /build/libs/revanced-patcher-*.jar 1> /dev/null 2>&1; then
+	cd /build/revanced-patcher || exit 1
+	chmod +x ./gradlew
+	printf '\nRunning build...\n'
+	./gradlew publish
+	printf '\nRelocating build asset(s)...\n'
+	mv ./build/libs/revanced-patcher-*.jar /build/libs/
+	printf '\nDone.\n'
+else
+	printf '\nrevanced-patcher already built. Skipping build.\n'
+fi
 
 printf '\n===============================\n== Building revanced-patches ==\n===============================\n'
-cd /build/revanced-patches || exit 1
-chmod +x ./gradlew
-printf '\nRunning build...\n'
-./gradlew build
-printf '\nRelocating build asset(s)...\n'
-mv ./build/libs/revanced-patches-*.jar /build/libs/
-printf '\nDone.\n'
+if ! ls /build/libs/revanced-patches-*.jar 1> /dev/null 2>&1; then
+	cd /build/revanced-patches || exit 1
+	chmod +x ./gradlew
+	printf '\nRunning build...\n'
+	./gradlew build
+	printf '\nRelocating build asset(s)...\n'
+	mv ./build/libs/revanced-patches-*.jar /build/libs/
+	printf '\nDone.\n'
+else
+	printf '\nrevanced-patches already built. Skipping build.\n'
+fi
 
 printf '\n===========================\n== Building revanced-cli ==\n===========================\n'
-cd /build/revanced-cli || exit 1
-chmod +x ./gradlew
-printf '\nRunning build...\n'
-./gradlew build
-printf '\nRelocating build asset(s)...\n'
-mv ./build/libs/revanced-cli-*.jar /build/libs/
-printf '\nDone.\n'
+if ! ls /build/libs/revanced-cli-*-all.jar 1> /dev/null 2>&1; then
+	cd /build/revanced-cli || exit 1
+	chmod +x ./gradlew
+	printf '\nRunning build...\n'
+	./gradlew build
+	printf '\nRelocating build asset(s)...\n'
+	mv ./build/libs/revanced-cli-*.jar /build/libs/
+	printf '\nDone.\n'
+else
+	printf '\nrevanced-cli already built. Skipping build.\n'
+fi
 
 printf '\n==========================\n== Patching YouTube APK ==\n==========================\n'
 printf '\nFinding dependendant files...\n'
@@ -46,25 +58,35 @@ cli_jar=$(find . -name "*.jar" | grep -oE "revanced-cli-[[:digit:]]\.[[:digit:]]
 patches_jar=$(find . -name "*.jar" | grep -oE "revanced-patches-([[:digit:]]\.){1,3}jar")
 
 for var in "$yt_apk" "$integrations_apk" "$cli_jar" "$patches_jar"; do
-        if [ -z "$var" ]; then
-          printf 'Error: dependency not found. Please ensure the following files exist in ./build/libs/:'
-          printf '\n  * *youtube*.apk'
-          printf '\n  * revanced-integrations.apk'
-          printf '\n  * revanced-cli-*.jar'
-          printf '\n  * revanced-patches-*.jar'
+	if [ -z "$var" ]; then
+		printf 'Error: dependency not found. Please ensure the following files exist in ./build/libs/:'
+		printf '\n  * *youtube*.apk'
+		printf '\n  * revanced-integrations.apk'
+		printf '\n  * revanced-cli-*-all.jar'
+		printf '\n  * revanced-patches-*.jar'
+		printf '\n  * revanced-patcher-*.jar'
+	fi
 done
 
 if [ ! -d revanced-cache ] ; then
-  printf '\nCreating directory: revanced-cache\n'
-  mkdir revanced-cache
+	printf '\nCreating directory: revanced-cache\n'
+	mkdir revanced-cache
 fi
 
-printf '\nPatching APK...\n'
+printf '\nPatching APK...\n\n'
 java -jar "$cli_jar" \
-  --patches="$patches_jar" \
-  --merge="../$integrations_apk" \
-  --apk="../$yt_apk" \
-  --out="../revanced.apk" \
-  --temp-dir="revanced-cache" \
-  --clean &&\
-printf '\nDone.\nYour patched APK can be found at: ./build/revanced.apk\n'
+	--patches="$patches_jar" \
+	--merge="../$integrations_apk" \
+	--apk="../$yt_apk" \
+	--out="revanced.apk" \
+	--temp-dir="revanced-cache"
+
+printf '\nPlease wait...\n'
+
+if [ -f ./revanced.apk ] ; then
+	mv ./revanced.apk /build/
+	rm -rf revanced-cache
+	printf '\nDone.\nYour patched APK can be found at: ./build/revanced.apk\n'
+else
+	printf '\nPatch failed; no file outputted. Please retry.\n'
+fi
